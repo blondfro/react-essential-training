1fbeb8b4e7d9d7f9c3bd74dd969e69f5a7b00b8b
James Morris <blondfro@gmail.com>
2020-03-13 17:05:59 -0500

    created 2 methods for calculating goal progress.

--- src/components/SkiDayCounter.js
+++ src/components/SkiDayCounter.js
@@ -1,13 +1,21 @@
 import React from "react";
 
 function SkiDayCounter({ total, powder, backCountry, goal }) {
+  const getPercent = decimal => {
+    return decimal * 100 + "%";
+  };
+
+  const caclGoalProgress = (total, goal) => {
+    return getPercent(total / goal);
+  };
+
   return (
     <div>
       <section>
         <div>Total Days: {total}</div>
         <div>Powder Days: {powder}</div>
         <div>Back Country Days: {backCountry}</div>
-        <div>Goal: {goal}</div>
+        <div>Goal Progress: {caclGoalProgress(total, goal)}</div>
       </section>
     </div>
   );
