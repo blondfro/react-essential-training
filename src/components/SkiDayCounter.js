1c5fa7141c27f642cfa106e4b621dc342a162fc8
James Morris <blondfro@gmail.com>
2020-03-13 17:35:39 -0500

    testing mapping over some data. created sample data from a previous project.

--- src/components/SkiDayCounter.js
+++ src/components/SkiDayCounter.js
@@ -5,7 +5,7 @@ function SkiDayCounter({ total, powder, backCountry, goal, skiDays }) {
     return decimal * 100 + "%";
   };
 
-  const caclGoalProgress = (total, goal) => {
+  const calcGoalProgress = (total, goal) => {
     return getPercent(total / goal);
   };
 
@@ -15,7 +15,7 @@ function SkiDayCounter({ total, powder, backCountry, goal, skiDays }) {
         <div>Total Days: {total}</div>
         <div>Powder Days: {powder}</div>
         <div>Back Country Days: {backCountry}</div>
-        <div>Goal Progress: {caclGoalProgress(total, goal)}</div>
+        <div>Goal Progress: {calcGoalProgress(total, goal)}</div>
         <ul>
           {skiDays.map((day, i) => (
             <li key={i}>{day.resort}</li>
